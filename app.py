b83a42c75bd20c7ac22ffbac23ac7001ccc75b6d
Olive-Roe <14oloh@kl.his.edu.my>
2022-04-06 22:55:36 +0800

    Clean up imports

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import engineDEV
-import threading
-import os
-import time
+from threading import Thread
+from os import system
+from time import sleep
 from turbo_flask import Turbo
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ outputBoard = engineDEV.outputBoard
 
 
 def runGame():
-    os.system("python3 -m engineDEV")
+    system("python3 -m engineDEV")
 
 
 def start_flask_app():
@@ -21,7 +21,7 @@ def start_flask_app():
 def update():
     with app.app_context():
         while True:
-            time.sleep(1)
+            sleep(1)
             with open("logfiles/xmlboard.txt", "r") as f:
                 svg = f.read()
             with open("logfiles/livePGN.txt", "r") as f:
@@ -40,21 +40,13 @@ def clear_tempfiles():
 
 @app.route("/")
 def index():
-    # return render_template("index.html", board=svg, livePGN=pgn, evaluation=currentEval)
     return render_template("index.html")
 
 
-@app.route("/xmlboard.txt")
-def xmlboard():
-    if request.method == "GET":
-        with open("logfiles/xmlboard.txt", "r") as f:
-            return f.read()
-
-
 clear_tempfiles()
-t1 = threading.Thread(target=runGame)
-t2 = threading.Thread(target=start_flask_app)
-t3 = threading.Thread(target=update)
+t1 = Thread(target=runGame)
+t2 = Thread(target=start_flask_app)
+t3 = Thread(target=update)
 t1.start()
 t2.start()
 t3.start()
