48c34260a58740924cb3c32c93b6e133284ec702
Olive-Roe <14oloh@kl.his.edu.my>
2022-04-06 22:23:07 +0800

    Add turboflask integration
    Cleaner communication

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request
 import engineDEV
 import threading
 import os
+import time
+from turbo_flask import Turbo
 
 app = Flask(__name__)
+turbo = Turbo(app)
 outputBoard = engineDEV.outputBoard
 
 
@@ -15,25 +18,36 @@ def start_flask_app():
     app.run(debug=False)
 
 
-t1 = threading.Thread(target=runGame)
-t2 = threading.Thread(target=start_flask_app)
-t1.start()
-t2.start()
+def update():
+    with app.app_context():
+        while True:
+            time.sleep(1)
+            with open("logfiles/xmlboard.txt", "r") as f:
+                svg = f.read()
+            with open("logfiles/livePGN.txt", "r") as f:
+                pgn = f.read()
+            with open("logfiles/evaluation.txt", "r") as f:
+                currentEval = f.read()
+            turbo.push([turbo.update(svg, 'board'), turbo.update(
+                pgn, 'pgn'), turbo.update(currentEval, 'eval')])
 
 
 @app.route("/")
 def index():
-    with open("logfiles/xmlboard.txt", "r") as f:
-        svg = f.read()
-    with open("logfiles/livePGN.txt", "r") as f:
-        pgn = f.read()
-    with open("logfiles/evaluation.txt", "r") as f:
-        currentEval = f.read()
-    return render_template("index.html", board=svg, livePGN=pgn, evaluation=currentEval)
+    # return render_template("index.html", board=svg, livePGN=pgn, evaluation=currentEval)
+    return render_template("index.html")
 
 
 @app.route("/xmlboard.txt")
-def index():
+def xmlboard():
     if request.method == "GET":
         with open("logfiles/xmlboard.txt", "r") as f:
             return f.read()
+
+
+t1 = threading.Thread(target=runGame)
+t2 = threading.Thread(target=start_flask_app)
+t3 = threading.Thread(target=update)
+t1.start()
+t2.start()
+t3.start()
